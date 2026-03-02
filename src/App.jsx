54e5bea9ff6b4bd75fdbe2d06405639b2f2878b2
Sonu Kumar <sonupvlog8937@gmail.com>
2026-03-02 22:31:36 +0530

    loader

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
-import React, { useEffect } from "react";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { useEffect } from "react";
+import { BrowserRouter, Route, Routes, useLocation } from "react-router-dom";
 import "./App.css";
 import "./responsive.css";
 import Header from "./components/Header";
@@ -25,6 +25,7 @@ import { OrderFailed } from "./Pages/Orders/failed";
 import SearchPage from "./Pages/Search";
 import Blog from "./Pages/Blog";
 import BlogDetails from "./Pages/BlogDetails";
+import CategoriesPage from "./Pages/Categories";
 
 import { useDispatch, useSelector } from "react-redux";
 import {
@@ -33,9 +34,63 @@ import {
   fetchMyListData,
   fetchUserDetails,
   setAddressMode,
+  setGlobalLoading,
   setIsLogin,
   setWindowWidth,
 } from "./store/appSlice";
+
+const GlobalLoader = () => {
+  const isLoading = useSelector((state) => state.app.globalLoading);
+
+  if (!isLoading) return null;
+
+  return (
+    <div className="global-loader-overlay" role="status" aria-live="polite" aria-label="Loading">
+      <div className="global-loader-spinner" />
+      <p>Loading...</p>
+    </div>
+  );
+};
+
+const AppContent = () => {
+  const dispatch = useDispatch();
+  const location = useLocation();
+
+  useEffect(() => {
+    dispatch(setGlobalLoading(true));
+    const timer = setTimeout(() => {
+      dispatch(setGlobalLoading(false));
+    }, 350);
+
+    return () => clearTimeout(timer);
+  }, [location.pathname, dispatch]);
+
+  return (
+    <>
+      <GlobalLoader />
+      <Header />
+      <Routes>
+        <Route path="/" exact={true} element={<Home />} />
+        <Route path="/products" exact={true} element={<ProductListing />} />
+        <Route path="/product/:id" exact={true} element={<ProductDetails />} />
+        <Route path="/login" exact={true} element={<Login />} />
+        <Route path="/register" exact={true} element={<Register />} />
+        <Route path="/cart" exact={true} element={<CartPage />} />
+        <Route path="/verify" exact={true} element={<Verify />} />
+        <Route path="/forgot-password" exact={true} element={<ForgotPassword />} />
+        <Route path="/checkout" exact={true} element={<Checkout />} />
+        <Route path="/my-account" exact={true} element={<MyAccount />} />
+        <Route path="/my-list" exact={true} element={<MyList />} />
+        <Route path="/my-orders" exact={true} element={<Orders />} />
+        <Route path="/order/success" exact={true} element={<OrderSuccess />} />
+        <Route path="/order/failed" exact={true} element={<OrderFailed />} />
+        <Route path="/address" exact={true} element={<Address />} />
+        <Route path="/search" exact={true} element={<SearchPage />} />
+      </Routes>
+      <Footer />
+    </>
+  );
+};
 
 function App() {
   const dispatch = useDispatch();
@@ -75,28 +130,7 @@ function App() {
   return (
     <>
       <BrowserRouter>
-        <Header />
-        <Routes>
-          <Route path={"/"} exact={true} element={<Home />} />
-          <Route path={"/products"} exact={true} element={<ProductListing />} />
-          <Route path={"/product/:id"} exact={true} element={<ProductDetails />} />
-          <Route path={"/login"} exact={true} element={<Login />} />
-          <Route path={"/register"} exact={true} element={<Register />} />
-          <Route path={"/cart"} exact={true} element={<CartPage />} />
-          <Route path={"/verify"} exact={true} element={<Verify />} />
-          <Route path={"/forgot-password"} exact={true} element={<ForgotPassword />} />
-          <Route path={"/checkout"} exact={true} element={<Checkout />} />
-          <Route path={"/my-account"} exact={true} element={<MyAccount />} />
-          <Route path={"/my-list"} exact={true} element={<MyList />} />
-          <Route path={"/my-orders"} exact={true} element={<Orders />} />
-          <Route path={"/order/success"} exact={true} element={<OrderSuccess />} />
-          <Route path={"/order/failed"} exact={true} element={<OrderFailed />} />
-          <Route path={"/address"} exact={true} element={<Address />} />
-          <Route path={"/search"} exact={true} element={<SearchPage />} />
-          <Route path={"/blog"} exact={true} element={<Blog />} />
-          <Route path={"/blog/:id"} exact={true} element={<BlogDetails />} />
-        </Routes>
-        <Footer />
+        <AppContent />
       </BrowserRouter>
 
 
