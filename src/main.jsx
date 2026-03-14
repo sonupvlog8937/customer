21297cdbc1e9968fcd4a5e61b037b864f563ff91
Sonu Kumar <sonupvlog8937@gmail.com>
2026-03-14 16:18:45 +0530

    hfg

--- src/main.jsx
+++ src/main.jsx
@@ -3,19 +3,6 @@ import './index.css';
 import App from './App.jsx';
 import { Provider } from 'react-redux';
 import { store } from './store';
-import { registerSW } from 'virtual:pwa-register';
-
-// Register Service Worker with auto-update
-const updateSW = registerSW({
-  onNeedRefresh() {
-    if (confirm('Naya update available hai! Refresh karein?')) {
-      updateSW(true);
-    }
-  },
-  onOfflineReady() {
-    console.log('App ab offline bhi kaam karega!');
-  },
-});
 
 createRoot(document.getElementById('root')).render(
     <Provider store={store}>
