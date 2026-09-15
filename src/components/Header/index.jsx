04396c65cdae286856f3e6c750d1361f96f01a29
Sonu Kumar <youremail@gmail.com>
2026-09-15 18:26:27 +0530

    hkjl

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -765,12 +765,7 @@ const Header = () => {
 
             {/* Desktop: Search Bar */}
             <div className="col2 hidden lg:block flex-1 lg:w-[36%]">
-              <Search 
-                onSearchComplete={() => {
-                  // Desktop inline search - just close dropdown, no modal
-                  console.log("🔴 Desktop inline search completed");
-                }}
-              />
+              <Search />
             </div>
 
             {/* Right Side Actions */}
